bc27ea1a89ddc926d311f0b1bca7defa9a943fc0
Henrique Geenen <henriqueggo@hotmail.com>
2024-11-28 22:38:24 -0300

    changes

---
+++ src/main/java/com/techchallenge4/ms_logistica/configuration/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.techchallenge4.ms_logistica.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("logistica", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+
+}
